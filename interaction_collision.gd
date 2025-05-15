3082d44c7c792ee3b36a6373fb6ffd14787c4957
Godot-Yr12 <19mukwayae@steds.org.uk>
2025-05-15 12:21:26 +0100

    Dev_1.2_JSON+Advance_dialogue

---
+++ interaction_collision.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+var dialogue_scene = preload("res://dialogue.tscn")
+var dialogue = dialogue_scene.instantiate()
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	add_child(dialogue)
+	dialogue.position.y = Player.position.y -78
+	print("interaction")
+	Player.velocity.x == 0 and Player.velocity.y == 0
+	pass # Replace with function body.
+
+
+func _on_body_exited(body: CharacterBody2D) -> void:
+	remove_child(dialogue)
+	pass # Replace with function body.
